e67abec6b1a2f4c88e5c01d00987593821384120
dawit <dawit@gmail.com>
2023-10-20 10:15:41 +0300

    an input field added in this commit

--- World/src/Components/Worldcount.jsx
+++ World/src/Components/Worldcount.jsx
@@ -4,6 +4,7 @@ const url = 'https://restcountries.com/v2/all';
 
 const Worldcount = () => {
   const [country, setCountry] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchWorldCountryData = async () => {
@@ -20,38 +21,49 @@ const Worldcount = () => {
     fetchWorldCountryData();
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCountries = country.filter((country) =>
+    country.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <section className='table'>
-      <table>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a country name here"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <table className="world-table">
         <thead>
           <tr>
-            <th className="column-divider"><div className="header-line"></div>Flag</th>
-            <th className="column-divider"><div className="header-line"></div>Country</th>
-
-            
-
-            <th className="column-divider"><div className="header-line"></div>Capital</th>
-            <th className="column-divider"><div className="header-line"></div>Population</th>
-            <th className="column-divider"><div className="header-line"></div>Language</th>
+            <th className="column-divider">Flag</th>
+            <th className="column-divider">Country</th>
+            <th className="column-divider">Capital</th>
+            <th className="column-divider">Population</th>
+            <th className="column-divider">Language</th>
           </tr>
-          <tr><div className="header-line"></div></tr>
-          
         </thead>
-        <tbody >
-          {country.map((country, index) => {
+        <tbody>
+          {filteredCountries.map((country, index) => {
             const { name, flags, capital, population, languages } = country;
             const flag = flags.png;
 
             return (
               <React.Fragment key={name}>
-                <tr >
+                <tr>
                   <td className="column-divider"><img src={flag} alt={name} /></td>
                   <td className="column-divider">{name}</td>
                   <td className="column-divider">{capital}</td>
                   <td className="column-divider">{population}</td>
                   <td className="column-divider">{languages[0].name}</td>
                 </tr>
-                {index !== country.length - 1 && <tr><td colSpan="5" className="row-divider"></td></tr>}
+                {index !== filteredCountries.length - 1 && <tr><td colSpan="5" className="row-divider"></td></tr>}
               </React.Fragment>
             );
           })}
